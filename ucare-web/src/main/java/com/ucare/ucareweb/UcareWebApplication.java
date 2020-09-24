aa14bf9da92624efccae9ec1dfc5558c6ea3b4e2
Eike Konuma <eeikee@192.168.11.10>
2020-09-24 18:59:41 -0300

    add doc with LandingPage and ucare-web backend

---
+++ ucare-web/src/main/java/com/ucare/ucareweb/UcareWebApplication.java
@@ -0,0 +1,13 @@
+package com.ucare.ucareweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcareWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcareWebApplication.class, args);
+	}
+
+}
---
+++ ucare-web/src/test/java/com/ucare/ucareweb/UcareWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucare.ucareweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcareWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
