8e496cd1690c5c0becabaf12515ebd518872e6a7
luizeduardo042 <59454193+luizeduardo042@users.noreply.github.com>
2020-09-29 20:13:59 -0300

    Update ucare.sql

--- DB/ucare.sql
+++ DB/ucare.sql
@@ -1,2 +1,36 @@
-create database ucare;
-use ucare;
+use heroku_5534e621f769bf1;
+
+create table pacientes(
+	id			integer PRIMARY KEY AUTO_INCREMENT,
+    nome		varchar(30)		not null,
+    sobrenome	varchar(30)		not null,
+    data_nasc	date			not null,
+    cpf			varchar(14)		not null,
+    email		varchar(30)		not null,
+    telefone	varchar(15)		not null,
+    estado		varchar(2)		not null,
+    cidade		varchar(15)		not null,
+    bairro		varchar(30)		not null,
+    rua			varchar(30)		not null,
+    numero		int				not null
+    
+);
+
+create table psicologos(
+	id			integer PRIMARY KEY AUTO_INCREMENT,
+    nome		varchar(30)		not null,
+    sobrenome	varchar(30)		not null,
+    data_nasc	date			not null,
+    cpf			varchar(14)		not null,
+    email		varchar(30)		not null,
+    telefone	varchar(15)		not null,
+    estado		varchar(2)		not null,
+    cidade		varchar(15)		not null,
+    bairro		varchar(30)		not null,
+    rua			varchar(30)		not null,
+    numero		int				not null
+    
+    
+    
+);
+
