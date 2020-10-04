323e89e67764c7a7ea574833bcf72438f951166c
luizeduardo042 <luiz.eduardo.200@hotmail.com>
2020-10-04 19:00:36 -0300

    updateBD

--- DB/ucare.sql
+++ DB/ucare.sql
@@ -5,27 +5,24 @@ use heroku_5534e621f769bf1;
 create table pacientes(
 	id			integer PRIMARY KEY AUTO_INCREMENT,
     nome		varchar(30),
-    sobrenome	varchar(30),
-    data_nasc	date,
+    idade		int,
     cpf			varchar(14),
+    cep			varchar(10),
     email		varchar(30),
-    telefone	varchar(15),
     estado		varchar(2),
     cidade		varchar(15),
     bairro		varchar(30),
     rua			varchar(30),
     numero		int
-    
 );
 
 create table psicologos(
 	id			integer PRIMARY KEY AUTO_INCREMENT,
     nome		varchar(30),
-    sobrenome	varchar(30),
-    data_nasc	date,
+    idade		int,
     cpf			varchar(14),
+    cep			varchar(10),
     email		varchar(30),
-    telefone	varchar(15),
     estado		varchar(2),
     cidade		varchar(15),
     bairro		varchar(30),
@@ -40,9 +37,9 @@ create table consultas(
     id           integer PRIMARY KEY AUTO_INCREMENT,
     paciente      varchar(30),
     psicologo     varchar(30),
-    data_cons    date,
-    horario      time,
-    plataforma   varchar(30)
+    data_cons     date,
+    horario       time,
+    plataforma    varchar(30)
 
 
 );
@@ -50,12 +47,13 @@ create table consultas(
 
 
 
-insert into psicologos(nome,sobrenome,data_nasc,cpf,email,telefone,estado,cidade,bairro,rua,numero) values ('DR. Joaquim','Lopes Ferreira','1984-07-09','474.518.998-29','joaquim.lopes@hotmail.com','15996302175','SP','Sorocaba','Wanel Ville','Valmir vitório segura','100');
+insert into psicologos(nome,idade,cpf,cep,email,estado,cidade,bairro,rua,numero) values ('DR. Eike','19','xxx.xxx.xxx-xx','xxxxx-xxx','eike@hotmail.com','SP','Sorocaba','São bento','xxxxx','xxxx');
 
-insert into pacientes(nome,sobrenome,data_nasc,cpf,email,telefone,estado,cidade,bairro,rua,numero) values ('Joaquim','Lopes Ferreira','1984-07-09','474.518.998-29','joaquim.lopes@hotmail.com','15996302175','SP','Sorocaba','Wanel Ville','Valmir vitório segura','100');
+insert into pacientes(nome,idade,cpf,cep,email,estado,cidade,bairro,rua,numero) values ('Luiz Eduardo','19','474.518.998-29','18053-525','luiz.eduardo.200@hotmail.com','SP','Sorocaba','Wanel Ville','Valmir vitório segura','100');
+insert into pacientes(nome,idade,cpf,cep,email,estado,cidade,bairro,rua,numero) values ('Angelina','19','xxx.xxx.xxx-xx','xxxxx-xxx','angelina@hotmail.com','SP','Sorocaba','Jd.São Guilherme','XXXXXX','xxxx');
 
-insert into consultas(paciente,psicologo,data_cons,horario,plataforma) values ('Joaquim','DR.Joaquim','2020-08-09','15:30','Discord');
-
+insert into consultas(paciente,psicologo,data_cons,horario,plataforma) values ('Luiz Eduardo','DR. Eike','2020-08-09','15:30','Discord');
+insert into consultas(paciente,psicologo,data_cons,horario,plataforma) values ('Angelina','DR. Eike','2020-09-10','14:20','Zoom');
 select * from psicologos;
 select * from pacientes;
 select * from consultas;
