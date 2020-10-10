ae18c7b73f21d931a5b7e170b53bd263d3c23417
eeikee <konumaeike5618@hotmail.com>
2020-10-10 12:35:22 -0300

    add cpf and cep varification api

--- web-ucare/src/main/java/com/ucare/webucare/model/User.java
+++ web-ucare/src/main/java/com/ucare/webucare/model/User.java
@@ -1,4 +1,26 @@
 package com.ucare.webucare.model;
 
+import br.com.caelum.stella.validation.CPFValidator;
+import com.ucare.webucare.model.cep.ViaCEP;
+import com.ucare.webucare.model.cep.ViaCEPException;
+import org.json.JSONException;
+
 public class User {
+    String nomeCompleto;
+    int idade;
+    String cpf;
+    String cep;
+
+
+    public static void main(String[] args) throws JSONException, ViaCEPException {
+        ViaCEP viacep = new ViaCEP();
+        viacep.buscar(cep);
+        System.out.println(viacep.getBairro()+ " - " + viacep.getLogradouro());
+
+    }
+
+    public boolean valida(String cfp){
+        CPFValidator cpfValidator = new CPFValidator();
+        cpfValidator.assertValid(cpf);
+    }
 }
